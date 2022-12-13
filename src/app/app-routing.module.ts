d689223474e5bf5049e182125dab921ca86e1ec4
Luka772000 <lukabrasnic772000@gmail.com>
2022-12-13 15:57:38 +0100

    Chapter10End

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailsComponent } from './shop/product-details/product-details.component';
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule) },
+  {path:'**', redirectTo: 'shop', pathMatch: 'full' },
+  
+
+  // { path: '', redirectTo: 'shop', pathMatch: 'full' },
+  // { path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule) },
+  // { path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule) },
+  // { path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule) },
+  // { path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) },
+  // { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
+  // { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  // { path: '**', redirectTo: 'shop', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
-    ShopModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent],
